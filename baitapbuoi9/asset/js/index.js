34ba6f4a41a79d6add7c1de52eb5a4ec46b887d0
LE NGOC LAM NA <nalengoclam@gmail.com>
2025-01-19 23:08:24 +0700

    [feat]=add input and paging

--- baitapbuoi9/asset/js/index.js
+++ baitapbuoi9/asset/js/index.js
@@ -1,21 +1,241 @@
-const container=document.querySelector(".pokemon-container");
 
-function dispalayPokemoon(data){
-    const pokemonList=data.results;
-    pokemonList.forEach((pokemon,index) => {
-        const id=index+1;
-        const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
-    
-        const pokemonCard=document.createElement("div");
-        pokemonCard.className="pokemon-card ";
-        pokemonCard.innerHTML=`
-        <p>#${id}</p>
-        <img src="${imageUrl}" alt="${pokemon.name}">
-        <h2>${pokemon.name}</h2>
-        `;
 
-        container.appendChild(pokemonCard);
-    });
+// const app= document.querySelector('#app')
+// const button=document.querySelector('button')
+// const input=document.querySelector('input')
+// const container=document.querySelector(".pokemon-container");
+
+// //ham lay api
+// function fetchPromise(URL){
+//     return new Promise(
+//         function(resolve){//ẩn của hàm callback , thnahf công thì gọi hàm sau .then
+//             fetch(URL)
+//             .then(
+//                 function(responsive){
+//                     const promise=responsive.json()
+//                     resolve(promise)//thành ccoong thì gọi hàm .then của cái sau/ tương tự bắt lỗi thì .catchcatch
+//                 }
+//             )
+//             .catch(function(error){
+//                 console.log(error)
+//                 app.innerHTML='Oh no! Some thing went wrong.'
+//             }
+//             )
+//         }
+//     )
+// }
+
+// let offset=0
+// const limit=36
+
+// let pokemons=JSON.parse(localStorage.getItem('pokemonsData'))
+// let filteredPokemon=pokemons
+// let pokemonType=[]
+
+// if(pokemons&& Array.isArray(pokemons)&& pokemons.length){
+//     render()
+// }
+// else{
+//     fetchPromise(`https://pokeapi.co/api/v2/pokemon/?offset=0&limit=898`)
+//         .then(
+//             function(value){
+//                 container.innerHTML=''
+//                 pokemons=value.results
+//                 filteredPokemon = value.results
+
+//                 localStorage.setItem('pokemonsData',JSON.stringify(pokemons))
+
+//                 pokemonType = pokemons.map(pokemon=>
+//                     fetchPromise(pokemon.url)
+//                 )
+//                 return Promise.all(pokemonType)
+//             }
+//         )
+//         .then(results => {
+//             // Thêm chi tiết vào mỗi Pokémon
+//         results.forEach((details, index) => {
+//             pokemons[index].details = {
+//                 types: details.types.map(type => type.type.name),
+//                 id: details.id,
+//             };
+//         });
+//         filteredPokemon = pokemons; // Cập nhật bộ lọc
+//         render();
+//             render();
+//         })
+// }
+
+// function render(){
+//     const renderLimit=offset+limit
+//     for(;offset<renderLimit;offset++){
+//         const pokemon=filteredPokemon[offset]
+//         if(!pokemon){
+//             button.style.display='none'
+//         }
+//         else{
+//             button.style.display='block'
+//         }
+
+//         // const div=document.createElement('div')
+//         // div.innerText=pokemon.name
+//         // app.appendChild(div)
+
+//         const id = pokemon.details?.id || 'Unknown';  
+//         const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+
+//         const pokemonCard=document.createElement("div");
+//         pokemonCard.className="pokemon-card ";
+//         pokemonCard.innerHTML=`
+//         <p>#${id}</p>
+//         <img src="${imageUrl}" alt="${pokemon.name}">
+//         <h2>${pokemon.name}</h2>
+//         <div class="types">
+//         ${pokemon.details?.types
+//         .map(type => `<span class="type ${type}">${type}</span>`)
+//         .join('') || ''}
+// </div>
+
+//         `;
+
+//         container.appendChild(pokemonCard);
+//     }
+//     button.style.display = offset < filteredPokemon.length ? 'block' : 'none';
+
+// }
+
+// button.addEventListener('click',render)
+
+// input.addEventListener('input',function(){
+//     offset=0
+//     container.innerHTML=''
+//     filteredPokemon=pokemons.filter(
+//         function(pokemon){
+//             return pokemon.name.toLowerCase().includes(input.value.toLowerCase())
+//         }
+//     )
+//     render()
+// })
+
+const app = document.querySelector('#app')
+const button = document.querySelector('button')
+const input = document.querySelector('input')
+const container = document.querySelector(".pokemon-container");
+
+
+function fetchPromise(URL) {
+    return new Promise(
+        function (resolve) {
+            fetch(URL)
+                .then(
+                    // function(response) {
+                    //     const promise = response.json()
+                    //     resolve(promise)
+                    // }
+                    response => response.json()
+                )
+                .then(data => resolve(data))
+                .catch(function (error) {
+                    console.log(error)
+                    app.innerHTML = 'Oh no! Some thing went wrong.'
+                })
+        }
+    )
 }
 
-dispalayPokemoon(data)
+let offset = 0
+const limit = 36
+let pokemons = JSON.parse(localStorage.getItem('pokemonsData'))
+let filteredPokemon = pokemons
+let pokemonType = []
+
+if (pokemons && Array.isArray(pokemons) && pokemons.length) {
+    render()
+} else {
+    fetchPromise(`https://pokeapi.co/api/v2/pokemon/?offset=0&limit=898`)
+        .then(
+            function (value) {
+                container.innerHTML = ''
+                pokemons = value.results
+                filteredPokemon = value.results
+
+                localStorage.setItem('pokemonsData', JSON.stringify(pokemons))
+
+                // return Promise.all(pokemonType)
+            }
+        )
+        .then(results => {
+            // DO SOMETHING TO RESULT
+
+            render()
+        })
+}
+
+function render() {
+    const renderLimit = offset + limit
+    const currentPokemonPromises = []; // Lưu các Promise chỉ cho lần render hiện tại
+
+    for (; offset < renderLimit; offset++) {
+        const pokemon = filteredPokemon[offset]
+        if (!pokemon) {
+            button.style.display = 'none'
+            break
+        } else {
+            button.style.display = 'block'
+        }
+        // pokemonType.push(
+        //     fetchPromise(pokemon.url)
+        // )
+
+        const promise = fetchPromise(pokemon.url)
+        currentPokemonPromises.push(promise)//luu vao mang hien tai
+        pokemonType.push(promise)//luu tat ca promise vao pokemonType
+    }
+
+    Promise.all(currentPokemonPromises)
+        .then(result => {
+            result.forEach(pokemonData => {
+                const id = pokemonData.id
+                const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+                const pokemonCard = document.createElement("div");
+                pokemonCard.className = "pokemon-card ";
+                pokemonCard.innerHTML = `
+                <p>#${id}</p>
+                <div class="img" style="background-image: url(${imageUrl});></div>
+                <h2>${capitalize(pokemonData.name)}</h2>
+                <div class="types">
+                ${pokemonData.types
+                        .map(types => `<span class="type ${types.type.name}">${capitalize( types.type.name)}</span>`)
+                        .join('') || ''}
+                </div>
+                `;
+                container.appendChild(pokemonCard);
+
+            })
+        })
+        .catch(error => {
+            console.error("loi tai pokemon: ",error)
+        })
+    // const div = document.createElement('div')
+    // div.innerText = pokemon.name
+    // container.appendChild(div)
+
+}
+
+button.addEventListener('click', render)
+
+
+input.addEventListener('input', function () {
+    offset = 0
+    container.innerHTML = ''
+    filteredPokemon = pokemons.filter(
+        function (pokemon) {
+            return pokemon.name.includes(input.value)
+        }
+    )
+    render()
+})
+
+function capitalize(word){
+    if(!word) return ''
+    return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+}
